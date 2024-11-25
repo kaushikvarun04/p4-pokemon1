b58e5607127e4f94c2a3e63c6f4383bcc03c29cd
varunkaushik <kaushikvarun04@gmail.com>
2024-11-25 12:51:13 -0500

    idk mst is cooked for some reason

--- poke.cpp
+++ poke.cpp
@@ -24,8 +24,8 @@ double positiveInfinity = std::numeric_limits<double>::infinity();
 
 struct Coordinate
 {
-    uint64_t x_cord;
-    uint64_t y_cord;
+    int x_cord;
+    int y_cord;
     char terrain; // Coast, Land, Ocean
 };
 
@@ -47,7 +47,8 @@ struct dv
 struct pv
 {
     size_t parent;
-    pv(size_t a) : parent(a) {}
+    // need to fix
+    pv() : parent(100) {}
 };
 struct PrimDS
 {
@@ -60,43 +61,69 @@ class Pokemon
 {
     PrimDS prim;
     vector<Coordinate> coordinateList;
+
+public:
     void readInputs(istream &cin, string mode)
     {
         Coordinate c;
-        cin >> c.x_cord >> c.y_cord;
-        if (c.x_cord < 0 && c.y_cord < 0)
+        // there is probably a better way to do this
+        size_t totalCnt = 0;
+        size_t landCntr = 0;
+        size_t coastCntr = 0;
+        size_t oceanCntr = 0;
+        coordinateList.resize(totalCnt);
+        cin >> totalCnt;
+        while (landCntr + coastCntr + oceanCntr < totalCnt)
         {
-            c.terrain = 'O';
-        }
-        else if (c.x_cord == 0 || c.y_cord == 0)
-        {
-            c.terrain = 'C';
-        }
-        else
-        {
-            c.terrain = 'L';
+            cin >> c.x_cord >> c.y_cord;
+            if (c.x_cord < 0 && c.y_cord < 0)
+            {
+                c.terrain = 'O';
+                oceanCntr++;
+            }
+            else if (c.x_cord == 0 || c.y_cord == 0)
+            {
+                c.terrain = 'C';
+                coastCntr++;
+            }
+            else
+            {
+                c.terrain = 'L';
+                landCntr++;
+            }
+            coordinateList.push_back(c);
         }
         if (mode == "MST")
         {
-            size_t a = coordinateList.size();
-            prim.kv.resize(a);
-            prim.pv.resize(a);
-            prim.dv.resize(a);
+            prim.kv.resize(totalCnt);
+            prim.pv.resize(totalCnt);
+            prim.dv.resize(totalCnt);
+            if (landCntr > 1 && oceanCntr > 1 && coastCntr < 1)
+            {
+                cerr << "Cannot construct MST";
+                exit(1);
+            }
+            else
+            {
+                createMST();
+                printMST();
+            }
         }
     }
     void createMST()
     {
         prim.dv[0].distance = 0;
-        size_t bestLoc = 0; // might need to default initialize this
-        double bestDist = positiveInfinity;
+
         size_t trueCntr = 0;
         // Set starting point dv to 0.
         //  Loop v times (until every kv is true):
         while (trueCntr < coordinateList.size())
         {
+            size_t bestLoc = 0; // might need to default initialize this
+            double bestDist = positiveInfinity;
             // 1. From the set of vertices for which kv is false, select the vertex v having the
             // smallest tentative distance dv.
-            for (int i = 0; i < coordinateList.size(); ++i)
+            for (size_t i = 0; i < coordinateList.size(); ++i)
             {
 
                 if (prim.kv[i].visited == false)
@@ -114,9 +141,9 @@ class Pokemon
             // 3. For each vertex w adjacent to v for which kw is false, test whether dw is
             // greater than distance(v,w). If it is, set dw to distance(v,w) and set pw to v.
             char t = coordinateList[bestLoc].terrain;
-            for (int i = 0; i < coordinateList.size(); ++i)
+            for (size_t i = 0; i < coordinateList.size(); ++i)
             {
-                if (coordinateList[i].terrain == t && prim.kv[i].visited == false)
+                if ((coordinateList[i].terrain == t || t == 'C') && prim.kv[i].visited == false)
                 {
                     // will probably need to fix this
                     double x = coordinateList[bestLoc].x_cord - coordinateList[i].x_cord;
@@ -132,9 +159,30 @@ class Pokemon
         }
     }
 
-    void printMST(){
-        //do something
-        
+    void printMST()
+    {
+        double distance = 0;
+        vector<pair<size_t, size_t>> output;
+        for (size_t i = 0; i < coordinateList.size(); ++i)
+        {
+            distance += sqrt(prim.dv[i].distance);
+            for (size_t j = 0; j < coordinateList.size(); ++j)
+            {
+                if (prim.pv[j].parent == i)
+                {
+                    pair<size_t, size_t> p;
+                    p.first = min(i, j);
+                    p.second = max(i, j);
+                    output.push_back(p);
+                    break;
+                }
+            }
+        }
+        cout << distance << "\n";
+        for (size_t i = 0; i < output.size(); ++i)
+        {
+            cout << output[i].first << " " << output[i].second << "\n";
+        };
     }
 };
 
@@ -148,7 +196,6 @@ int main(int argc, char *argv[])
     opterr = false; // Let us handle all error output for command line options
     int choice;     // for every getopt
     int index = 0;  // for every getopt
-    bool verbose = false;
     string mode;
     // Only open if a filename is given
 
@@ -170,7 +217,7 @@ int main(int argc, char *argv[])
         case 'm':
         {
             mode = optarg;
-            if (mode != "MST" || mode != "FASTTSP" || mode != "OPTTSP")
+            if (mode != "MST" && mode != "FASTTSP" && mode != "OPTTSP")
             {
                 cerr << "Error: Invalid mode";
             }
@@ -178,8 +225,10 @@ int main(int argc, char *argv[])
         }
     }
 
+    Pokemon poke;
     if (mode == "MST")
     {
+        poke.readInputs(cin, mode);
         // do something
     }
 }
